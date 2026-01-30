690a33ddd77789451247b76527582721408f2d86
diyorbe-beep <marufzonkodirov0@gmail.com>
2026-01-30 21:14:05 +0500

    awd

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,12 +2,12 @@ import api from '../api/client';
 
 export const AuthService = {
   login: async (username, password) => {
-    const response = await api.post('/login', { username, password });
+    const response = await api.post('/auth/login', { username, password });
     return response.data;
   },
 
   register: async (userData) => {
-    const response = await api.post('/register', userData);
+    const response = await api.post('/auth/register', userData);
     return response.data;
   },
 
